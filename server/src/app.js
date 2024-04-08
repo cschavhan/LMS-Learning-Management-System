5ebd88712a751b924dc40b01839864388e383ea0
cschavhan <chandrakantchavhan01@gmail.com>
2024-04-09 00:28:30 +0530

    add express.js router

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import userRoutes from "../routes/user.routes.js";
 import { config } from "dotenv";
 config();
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(morgan("dev"));
 
 // route
+app.use("/api/user", userRoutes);
+
 app.all("*", (req, res) => {
   res.status(404).send("OOPS!! 404 page not found");
 });
