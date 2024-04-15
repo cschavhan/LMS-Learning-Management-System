171d9213491530e800a7bb7c02fd64788da95dbf
cschavhan <chandrakantchavhan01@gmail.com>
2024-04-15 23:48:40 +0530

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>;
 }
 
 export default App;
