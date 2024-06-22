28bb034496c089ec5fd9241d71e044b1497b6759
cschavhan <chandrakantchavhan01@gmail.com>
2024-06-23 00:57:30 +0530

    created course creation api route

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,4 +1,5 @@
 import Course from "../models/course.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import AppError from "../utils/error.utils.js";
 
 export const getAllCourses = async (req, res, next) => {
@@ -37,5 +38,47 @@ export const getLecturesByCourseId = async (req, res, next) => {
     });
   } catch (error) {
     return next(new AppError(error.message, 500));
+  }
+};
+
+// create courses
+export const createCourses = async (req, res, next) => {
+  try {
+    const { title, description, category, createdBy } = req.body;
+    if (!title || !description || !category || !createdBy) {
+      return next(new AppError("All fields are required", 400));
+    }
+
+    // thumbnail upload
+    const thumbnailLocalPath = req.file?.path;
+    if (!thumbnailLocalPath) {
+      return next(new AppError("thumbnailLocalPath file is required", 400));
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    if (!thumbnail) {
+      return next(new AppError("thumbnail file is required", 400));
+    }
+
+    const course = await Course.create({
+      title,
+      description,
+      category,
+      createdBy,
+      thumbnail: thumbnail.url,
+    });
+
+    if (!course) {
+      return next(new AppError("Course creation failed,please try again", 400));
+    }
+
+    await course.save();
+    res.status(201).json({
+      success: true,
+      message: "Course created successfully",
+      course,
+    });
+  } catch (error) {
+    return next(new AppError("Course failed to create", 500));
   }
 };
--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
 import {
+  createCourses,
   getAllCourses,
   getLecturesByCourseId,
 } from "../controllers/course.controller.js";
 
 const router = express.Router();
 
+router.post("/", isLoggedIn, upload.single("thumbnail"), createCourses);
 router.get("/", getAllCourses);
 router.get("/:id", isLoggedIn, getLecturesByCourseId);
 
