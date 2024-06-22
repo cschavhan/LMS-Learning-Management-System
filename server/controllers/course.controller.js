4728139e7b392004715bf15d23d2debd8c20b7c6
cschavhan <chandrakantchavhan01@gmail.com>
2024-06-22 23:54:04 +0530

    created get lectures by course id api route

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -19,3 +19,23 @@ export const getAllCourses = async (req, res, next) => {
     );
   }
 };
+
+// get lecture by course id
+export const getLecturesByCourseId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const course = await Course.findById(id);
+    if (!course) {
+      return next(new AppError("Invalid course id", 400));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Course lectures fetched successfully",
+      lectures: course.lectures,
+    });
+  } catch (error) {
+    return next(new AppError(error.message, 500));
+  }
+};
--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getAllCourses } from "../controllers/course.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+  getAllCourses,
+  getLecturesByCourseId,
+} from "../controllers/course.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllCourses);
+router.get("/:id", isLoggedIn, getLecturesByCourseId);
 
 export default router;
