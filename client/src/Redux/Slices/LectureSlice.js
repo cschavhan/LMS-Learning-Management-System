edf15c3f033266c65a307f0814aef8ba01f0fb99
cschavhan <chandrakantchavhan01@gmail.com>
2024-06-27 18:32:52 +0530

    created async thunk for getting the course lectures

--- client/src/Redux/Slices/LectureSlice.js
+++ client/src/Redux/Slices/LectureSlice.js
@@ -1,7 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import axiosInstance from "../../Helpers/axiosInstance";
+
 const initialState = {
   lectureData: [],
 };
+
+// get all lecture
+export const getCourseLectures = createAsyncThunk(
+  "course/lecture/get",
+  async (id) => {
+    try {
+      const response = axiosInstance.get(`/course/${id}`);
+      toast.promise(response, {
+        loading: "Wait, lectures fetching is in process",
+        success: (response) => {
+          return response?.data?.message;
+        },
+        error: "Failed to fetched lectures",
+      });
+
+      return (await response).data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
 
 const lectureSlice = createSlice({
   name: "lecture",
