c6de62b0a99e3c3d95a8064fc4c3ad45b6e6bcef
cschavhan <chandrakantchavhan01@gmail.com>
2024-07-01 00:10:22 +0530

    created buy subscription api route

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,7 +1,39 @@
+import User from "../models/user.model.js";
+import razorpay from "../src/server.js";
+import AppError from "../utils/error.utils.js";
+
 export const getRazorpayApiKey = async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Razorpay Api key",
     key: process.env.RAZORPAY_KEY_ID,
+  });
+};
+
+// buy subscription
+export const buySubscription = async (req, res, next) => {
+  const { id } = req.user;
+  const user = await User.findById(id);
+  if (!user) {
+    next(new AppError("Unauthorized, please login", 400));
+  }
+
+  if (user.role === "ADMIN") {
+    next(new AppError("Admin cannot purchase a sunscription", 400));
+  }
+
+  const subscription = await razorpay.subscriptions.create({
+    plan_id: process.env.RAZORPAY_PLAN_ID,
+    customer_notify: 1,
+  });
+
+  user.subscription.id = subscription.id;
+  user.subscription.status = subscription.status;
+
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Subscribed Successfully",
+    subscription_id: subscription.id,
   });
 };
--- server/routes/payment.routes.js
+++ server/routes/payment.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getRazorpayApiKey } from "../controllers/payment.controller.js";
+import {
+  buySubscription,
+  getRazorpayApiKey,
+} from "../controllers/payment.controller.js";
 import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.route("/razorpay-key").get(isLoggedIn, getRazorpayApiKey);
+router.route("/subscribe").post(isLoggedIn, buySubscription);
 
 export default router;
